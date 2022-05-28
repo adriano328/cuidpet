480e7fb4dc4cdf53613b291e1b89f66b244f40f8
adriano328 <lucasadrianodias@gmail.com>
2022-05-27 21:17:02 -0400

    Envio

--- lib/screens/pages/patient_screen.dart
+++ lib/screens/pages/patient_screen.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_cuidpet/models/units/patient.dart';
 import 'package:flutter_cuidpet/models/units/patient_service.dart';
 
+void main() {
+  runApp(patinent_screen());
+}
+
 class patinent_screen extends StatefulWidget {
   patinent_screen({Key? key}) : super(key: key);
 
@@ -11,20 +15,12 @@ class patinent_screen extends StatefulWidget {
 
 class _patinent_screenState extends State<patinent_screen> {
   TextEditingController cpfController = TextEditingController();
-
   TextEditingController nomeTutorController = TextEditingController();
-
   TextEditingController nomePacienteController = TextEditingController();
-
   TextEditingController idadeController = TextEditingController();
-
   TextEditingController sexoController = TextEditingController();
-
   TextEditingController racaController = TextEditingController();
-
   String sexo = "";
-
-  String sexo2 = "";
 
   final _formkey = GlobalKey<FormState>();
 
@@ -63,147 +59,190 @@ class _patinent_screenState extends State<patinent_screen> {
                         const SizedBox(
                           height: 20,
                         ),
-                        const Text('CPF Tutor: ',
-                            style: TextStyle(fontSize: 12)),
-                        TextFormField(
-                          controller: cpfController,
-                          decoration: InputDecoration(
-                              isDense: true,
-                              contentPadding: const EdgeInsets.all(10),
-                              border: OutlineInputBorder(
-                                  borderSide: const BorderSide(
-                                      width: 2, color: Colors.amber),
-                                  borderRadius: BorderRadius.circular(8))),
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Por favor, preencher o CPF do tutor';
-                            }
-                            return null;
-                          },
+                        Row(
+                          children: [
+                            const Text('CPF Tutor: ',
+                                style: TextStyle(fontSize: 10)),
+                            Expanded(
+                              child: TextFormField(
+                                controller: cpfController,
+                                decoration: InputDecoration(
+                                    isDense: true,
+                                    contentPadding: const EdgeInsets.all(10),
+                                    border: OutlineInputBorder(
+                                        borderSide: const BorderSide(
+                                            width: 2, color: Colors.amber),
+                                        borderRadius:
+                                            BorderRadius.circular(8))),
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Por favor, preencher o CPF do tutor';
+                                  }
+                                  return null;
+                                },
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 15,
+                            ),
+                            const Text('Nome tutor: ',
+                                style: TextStyle(fontSize: 10)),
+                            Expanded(
+                              child: TextFormField(
+                                controller: nomeTutorController,
+                                decoration: InputDecoration(
+                                    isDense: true,
+                                    contentPadding: const EdgeInsets.all(10),
+                                    border: OutlineInputBorder(
+                                        borderSide: const BorderSide(
+                                            width: 1, color: Colors.amber),
+                                        borderRadius:
+                                            BorderRadius.circular(8))),
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Por favor, preencher o nome do Tutor';
+                                  }
+                                  return null;
+                                },
+                              ),
+                            ),
+                          ],
                         ),
                         const SizedBox(
                           height: 15,
                         ),
-                        const Text('Nome tutor: ',
-                            style: TextStyle(fontSize: 12)),
-                        TextFormField(
-                          controller: nomeTutorController,
-                          decoration: InputDecoration(
-                              isDense: true,
-                              contentPadding: const EdgeInsets.all(10),
-                              border: OutlineInputBorder(
-                                  borderSide: const BorderSide(
-                                      width: 1, color: Colors.amber),
-                                  borderRadius: BorderRadius.circular(8))),
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Por favor, preencher o nome do Tutor';
-                            }
-                            return null;
-                          },
+                        Row(
+                          children: [
+                            const Text('Nome Paciente: ',
+                                style: TextStyle(fontSize: 10)),
+                            Expanded(
+                              child: TextFormField(
+                                controller: nomePacienteController,
+                                decoration: InputDecoration(
+                                    isDense: true,
+                                    contentPadding: const EdgeInsets.all(10),
+                                    border: OutlineInputBorder(
+                                        borderSide: const BorderSide(
+                                            width: 1, color: Colors.amber),
+                                        borderRadius:
+                                            BorderRadius.circular(8))),
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Por favor, preencher o nome do Paciente';
+                                  }
+                                  return null;
+                                },
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 15,
+                            ),
+                            const Text('Idade: ',
+                                style: TextStyle(fontSize: 10)),
+                            Expanded(
+                              child: TextFormField(
+                                controller: idadeController,
+                                decoration: InputDecoration(
+                                    isDense: true,
+                                    contentPadding: const EdgeInsets.all(10),
+                                    border: OutlineInputBorder(
+                                        borderSide: const BorderSide(
+                                            width: 1, color: Colors.amber),
+                                        borderRadius:
+                                            BorderRadius.circular(8))),
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Por favor, preencher a idade do paciente';
+                                  }
+                                  return null;
+                                },
+                              ),
+                            ),
+                          ],
                         ),
                         const SizedBox(
-                          height: 15,
+                          height: 10,
                         ),
-                        const Text('Nome Paciente: ',
-                            style: TextStyle(fontSize: 12)),
-                        TextFormField(
-                          controller: nomePacienteController,
-                          decoration: InputDecoration(
-                              isDense: true,
-                              contentPadding: const EdgeInsets.all(10),
-                              border: OutlineInputBorder(
-                                  borderSide: const BorderSide(
-                                      width: 1, color: Colors.amber),
-                                  borderRadius: BorderRadius.circular(8))),
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Por favor, preencher o nome do Paciente';
-                            }
-                            return null;
-                          },
-                        ),
-                        const SizedBox(
-                          height: 15,
-                        ),
-                        const Text('Idade: ', style: TextStyle(fontSize: 12)),
-                        TextFormField(
-                          controller: idadeController,
-                          decoration: InputDecoration(
-                              isDense: true,
-                              contentPadding: const EdgeInsets.all(10),
-                              border: OutlineInputBorder(
-                                  borderSide: const BorderSide(
-                                      width: 1, color: Colors.amber),
-                                  borderRadius: BorderRadius.circular(8))),
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Por favor, preencher a idade do paciente';
-                            }
-                            return null;
-                          },
+                        Padding(
+                          padding: const EdgeInsets.only(right: 200),
+                          child: Row(
+                            children: [
+                              const Text('Sexo: ',
+                                  style: TextStyle(fontSize: 10)),
+                              Expanded(
+                                child: RadioListTile(
+                                    title: const Text(
+                                      "Macho",
+                                      style: TextStyle(fontSize: 10),
+                                    ),
+                                    value: "Macho",
+                                    groupValue: sexo,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        sexo = (value ?? '') as String;
+                                      });
+                                    }),
+                              ),
+                              Expanded(
+                                child: RadioListTile(
+                                    title: const Text("Femea",
+                                        style: TextStyle(fontSize: 10)),
+                                    value: "Femea",
+                                    groupValue: sexo,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        sexo = (value ?? '') as String;
+                                      });
+                                    }),
+                              ),
+                            ],
+                          ),
                         ),
                         const SizedBox(
-                          height: 15,
+                          height: 5,
                         ),
-                        const Text('Sexo: ', style: TextStyle(fontSize: 12)),
-                        RadioListTile(
-                            title: const Text("Macho"),
-                            value: "Macho",
-                            groupValue: sexo,
-                            onChanged: (value) {
-                              setState(() {
-                                sexo = (value ?? '') as String;
-                              });
-                            }),
-                        RadioListTile(
-                            title: const Text("Femea"),
-                            value: "Femea",
-                            groupValue: sexo,
-                            onChanged: (value) {
-                              setState(() {
-                                sexo = (value ?? '') as String;
-                              });
-                            }),
-                        const SizedBox(
-                          height: 15,
-                        ),
-                        const Text('Raça: ', style: TextStyle(fontSize: 12)),
-                        TextFormField(
-                          controller: racaController,
-                          decoration: InputDecoration(
-                              isDense: true,
-                              contentPadding: const EdgeInsets.all(10),
-                              border: OutlineInputBorder(
-                                  borderSide: const BorderSide(
-                                      width: 1, color: Colors.amber),
-                                  borderRadius: BorderRadius.circular(8))),
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Por favor, preencher a raça do paciente';
-                            }
-                            return null;
-                          },
+                        const Text('Raça: ', style: TextStyle(fontSize: 10)),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 250),
+                          child: TextFormField(
+                            controller: racaController,
+                            decoration: InputDecoration(
+                                isDense: true,
+                                contentPadding: const EdgeInsets.all(10),
+                                border: OutlineInputBorder(
+                                    borderSide: const BorderSide(
+                                        width: 1, color: Colors.amber),
+                                    borderRadius: BorderRadius.circular(8))),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Por favor, preencher a raça do paciente';
+                              }
+                              return null;
+                            },
+                          ),
                         ),
                         const SizedBox(
                           height: 30,
                         ),
-                        ElevatedButton(
-                            onPressed: () {
-                              if (_formkey.currentState!.validate()) {
-                                Patient_service patient_service =
-                                    Patient_service();
-                                Patient patient = Patient(
-                                    cpf: cpfController.text,
-                                    nome_tutor: nomeTutorController.text,
-                                    nome_paciente: nomePacienteController.text,
-                                    idade: idadeController.text,
-                                    sexo: sexo,
-                                    raca: racaController.text);
-                                patient_service.addUnit(patient);
-                              }
-                            },
-                            child: const Text('Salvar')),
+                        Center(
+                          child: ElevatedButton(
+                              onPressed: () {
+                                if (_formkey.currentState!.validate()) {
+                                  Patient_service patient_service =
+                                      Patient_service();
+                                  Patient patient = Patient(
+                                      cpf: cpfController.text,
+                                      nome_tutor: nomeTutorController.text,
+                                      nome_paciente:
+                                          nomePacienteController.text,
+                                      idade: idadeController.text,
+                                      sexo: sexo,
+                                      raca: racaController.text);
+                                  patient_service.addUnit(patient);
+                                }
+                              },
+                              child: const Text('Salvar')),
+                        ),
                         const SizedBox(
                           height: 15,
                         ),
